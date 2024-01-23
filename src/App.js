c888aadd1745b5464f83f45cdf007256e0fc7a42
Zach Stange <zachary.stange17@gmail.com>
2024-01-22 18:41:54 -0600

    HTML Semantics and Meta tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Nav/>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
